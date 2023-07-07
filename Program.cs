5aef996869443bf5118083939ca9750af5e2ef1f
YOS0602 from Eileen <yoshitaka.t.biz@gmail.com>
2023-07-07 13:03:14 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace csharp_sample;
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("This is C# sample");
+    }
+}
